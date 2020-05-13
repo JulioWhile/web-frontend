435bc7425000c9de395a588cd71ababce5dcfb87
julio contreras <julio.contreras0429@gmail.com>
2020-05-12 23:22:53 -0700

    agregados crud a rutas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,17 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
 import { TiendasComponent } from './components/tiendas/tiendas.component';
+import { CrudTiendasComponent } from './components/crud/crud-tiendas/crud-tiendas.component';
+import { CrudUsuariosComponent } from './components/crud/crud-usuarios/crud-usuarios.component';
+import { CrudComputadorasComponent } from './components/crud/crud-computadoras/crud-computadoras.component';
 
 
 const routes: Routes = [
   { path: 'home', component: HomeComponent },
   { path: 'tiendas', component: TiendasComponent},
+  { path: 'crud-tiendas', component: CrudTiendasComponent},
+  { path: 'crud-usuarios', component: CrudUsuariosComponent},
+  { path: 'crud-computadoras', component: CrudComputadorasComponent},
   { path: '', pathMatch: 'full', redirectTo: 'home' },
   { path: '**', pathMatch: 'full', redirectTo: 'home' },
 ];
