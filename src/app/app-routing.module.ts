c1c473b2f3e67e0211aba0237c4fe0810777f785
julio contreras <julio.contreras0429@gmail.com>
2020-05-12 23:13:09 -0700

    carpetas home, tiendas, shared/navbar creadas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { TiendasComponent } from './components/tiendas/tiendas.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '/', component: HomeComponent },
+  { path: '/tiendas', component: TiendasComponent},
+  { path: '', pathMatch: 'full', redirectTo: 'home' },
+  { path: '**', pathMatch: 'full', redirectTo: 'home' },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
