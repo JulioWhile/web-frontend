6f18daf55f44693fe362653b4631264493831586
julio contreras <julio.contreras0429@gmail.com>
2020-05-13 02:38:20 -0700

    seccion tienda terminados

--- src/app/components/tiendas/tiendas.component.ts
+++ src/app/components/tiendas/tiendas.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { TiendasService } from 'src/app/services/tiendas.service';
 
 @Component({
   selector: 'app-tiendas',
@@ -6,10 +7,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./tiendas.component.css']
 })
 export class TiendasComponent implements OnInit {
-
-  constructor() { }
+  tiendas = [];
+  constructor(private tiendasService: TiendasService) { }
 
   ngOnInit(): void {
+    this.tiendasService.getTiendas().subscribe(tienda => {
+      tienda.forEach(element => {
+        this.tiendas.push(element);
+        console.log(this.tiendas);
+      });
+    });
   }
 
 }
